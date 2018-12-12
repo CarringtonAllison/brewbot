03d4f6ffe955b2bd71fb7f4c5e189f2a9d410714
Andrew Muscara <andrewmuscara@gmail.com>
2018-12-12 11:44:32 -0700

    deleted console logs on user routes and server

--- client/src/Pages/articleScrape/index.js
+++ client/src/Pages/articleScrape/index.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react'
 import API from "../../utils/API"
+import jwt_decode from 'jwt-decode'
 
 class Scrape extends Component {
     state = {
         results: "",
+        first_name: '',
+        last_name: '',
+        email: '',
+        errors: {}
     }
 
     scrapeArticles() {
@@ -15,6 +20,13 @@ class Scrape extends Component {
 
     componentDidMount() {
         this.scrapeArticles();
+        const token = localStorage.getItem("usertoken");
+        const decoded = jwt_decode(token)
+        this.setState({
+            first_name: decoded.first_name,
+            last_name: decoded.last_name,
+            email: decoded.email
+        })
     }
 
     render() {
